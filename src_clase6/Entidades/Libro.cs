068806ea9611470e20e9989583a2c575cbefe688
Enrique Thedy <ethedy@gmail.com>
2015-09-22 19:04:25 -0300

    Clase 8 Inicio

--- src_clase6/Entidades/Libro.cs
+++ src_clase6/Entidades/Libro.cs
@@ -4,16 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Entidades
 {
   public class Libro
   {
+    [Required(ErrorMessage = "El campo ISBN debe estar completo", AllowEmptyStrings = false)]
+    [StringLength(13, MinimumLength = 13, ErrorMessage = "El ISBN debe ser exactamente de 13 caracteres")]
     public string ISBN13 { get; set; }
 
     public string ISBN10 { get; set; }
 
     public string Editorial { get; set; }
 
+    [Required(ErrorMessage = "Alguna vez viste un libro sin titulo???")]
     public string Titulo { get; set; }
 
     public string Subtitulo { get; set; }
@@ -28,6 +33,7 @@ namespace Entidades
     /// </summary>
     public string Autores { get; set; }
 
+    [Range(typeof(decimal), "0", "1500")]
     public decimal? Precio { get; set; }
 
     public string PathImagen { get; set; }
--- src_clase6/MVCOOMB/Controllers/IngresosController.cs
+++ src_clase6/MVCOOMB/Controllers/IngresosController.cs
@@ -4,14 +4,63 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Entidades;
+using Data;
+
 namespace MvcOMB.Controllers
 {
   public class IngresosController : Controller
   {
     // GET: Ingresos
-    public ActionResult Index()
+    public ActionResult Nuevo()
     {
-      return View();
+      Libro nuevoLibro = new Libro();
+
+      return View(nuevoLibro);
+    }
+
+    [HttpPost]
+    public ActionResult AddNew(Libro nuevoLibro)
+    {
+      OMBContext ctx = DB.Contexto;
+
+      //  ValidarModelo(nuevoLibro);
+
+      if (ModelState.IsValid)
+      {
+        try
+        {
+          ctx.Libros.Add(nuevoLibro);
+          ctx.SaveChanges();
+
+          return View("Resultado");
+        }
+        catch (Exception ex)
+        {
+          return new HttpUnauthorizedResult();
+        }
+      }
+      else
+        return View("Nuevo", nuevoLibro);
+    }
+
+    private void ValidarModelo(Libro libro)
+    {
+      if (string.IsNullOrEmpty(libro.ISBN13))
+        ModelState.AddModelError("ISBN13", "El campo ISBN debe estar completo");
+      
+      if (string.IsNullOrEmpty(libro.Titulo))
+        ModelState.AddModelError("Titulo", "Alguna vez viste un libro sin titulo???");
+      else
+      {
+        if (libro.Titulo.ToUpper().Contains("XXX"))
+          ModelState.AddModelError("", "Esta libreria es bastante decente!!!");
+      }
+    }
+
+    private void ValidarModeloSolo(Libro libro)
+    {
+      
     }
   }
 }
